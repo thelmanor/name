8979013e18a911cb094ae19258d990915e1ec6ca
Thelma N <35433558+thelmanor@users.noreply.github.com>
2018-03-14 16:18:11 -0400

    Add files via upload

---
+++ Names.java
@@ -0,0 +1,13 @@
+/**********************************************
+ *          Lab #1a                                               
+ *         Nora Thelma                                        
+ *        Jan 24, 2018                                       
+ **********************************************/
+public class Names {
+	public static void main(String[] args) {
+			String firstName="Thelma";
+			String lastName= "Nora";
+			System.out.println("First name ="+firstName);
+			System.out.println ("Last name ="+lastName);
+	}
+}// end class 
